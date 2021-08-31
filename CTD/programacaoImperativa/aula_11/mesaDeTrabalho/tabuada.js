f68222be944e5eaebdbf049a8153a4fb1bb1bf9a
Ederson Diego de Oliveira <ederson.diego.oliveira@gmail.com>
2021-08-31 20:33:15 -0300

    Exercícios - Selecionando números impares e tabuada

---
+++ CTD/programacaoImperativa/aula_11/mesaDeTrabalho/tabuada.js
@@ -0,0 +1,6 @@
+for (let tab = 0; tab <= 10; tab++) {
+    for (let i = 0; i <= 10; i++) {
+        resultado = tab * i;
+        console.log("tabuada do" + tab + " = " + tab + " X " +  i  + " = " + resultado)   
+    }
+}
